7ac375bfdb26010502e1af7fdeb382315e53b323
fluttertutorialin <kamal.lakhani56@gmail.com>
2021-05-30 00:42:22 -0500

    SESSION COMMENT ADD

--- lib/shared/common/get_storage.dart
+++ lib/shared/common/get_storage.dart
@@ -2,22 +2,30 @@
    DEVELOPED BY: KAMLESH LAKHANI
    LOCAL STORAGE SESSION MANAGE
 */
+
 import 'package:get_storage/get_storage.dart';
 
 class Storage {
   GetStorage _getStorage;
+
   Storage(this._getStorage);
 
+  //ACCESS SESSION
   get getStorage => _getStorage;
 
+  //SESSION INSERT THE VALUE
   Future<void> saveValue(String key, dynamic value) =>
       _getStorage.writeIfNull(key, value);
 
+  //SESSION GET THE VALUE
   T? getValue<T>(String key) => _getStorage.read<T>(key);
 
+  //SESSION CHECK THE DATA IS AVAILABLE RETURN TRUE ELSE FALSE
   bool hasData(String key) => _getStorage.hasData(key);
 
+  //SESSION SINGLE REMOVE
   Future<void> removeValue(String key) => _getStorage.remove(key);
 
+  //SESSION CLEAR ALL DATA
   Future<void> clearStorage() => _getStorage.erase();
 }
